77c138f73d29da714c109821baf173dc9dccaae5
punjabinuclei <guppi0204@gmail.com>
2023-12-17 23:21:35 +0530

    Fix import statement for Redis client

--- server.js
+++ server.js
@@ -2,14 +2,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-imporecho "# cp-Calendar-Backend" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/punjabinuclei/cp-Calendar-Backend.git
-git push -u origin maint { createClient } from 'redis';
-
 
 const client = createClient();
 
