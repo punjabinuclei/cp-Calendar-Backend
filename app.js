b3a1b43dd27f45f259ed8335a0dacb5922ccb531
punjabinuclei <guppi0204@gmail.com>
2024-02-03 14:38:35 +0530

    PORT Made Dynamic

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cron from 'node-cron';
 import { connectToRedis, getCache, setCache } from './redis.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 import dotenv from 'dotenv';
 dotenv.config();
